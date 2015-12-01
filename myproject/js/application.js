53150c981fd8544707ba329c552b72ace4ed3ba5
Frezzley <dimitri.anderegg@semabit.ch>
2015-12-01 11:09:15 +0100

    cleaned code

--- myproject/js/application.js
+++ myproject/js/application.js
@@ -3,140 +3,45 @@ $(document).ready(function () {
         source: '/User/restaurantList',
         minLength: 2,
         select: function (event, ui) {
-           // $('input[name="restaurant"]')
             var restaurantexists = "false";
-            $('.restaurant').each(function(index, item) {
-            //   console.log($(item).val());
-            //    console.log(ui.item.value);
-                if(ui.item.value == $(item).val()){restaurantexists = "true";}});
-            if(restaurantexists == "false") {
+            $('.restaurant').each(function (index, item) {
+                if (ui.item.value == $(item).val()) {
+                    restaurantexists = "true";
+                }
+            });
+            if (restaurantexists == "false") {
                 var template = $('#template-hidden').clone();
-                //template.append('<input name="restaurant">');
-                //console.log(document.getElementById(ui.item.value).className += 'restaurant');
-//            template.className += 'restaurant';
-                // template.add(className += 'restaurant');
-                // document.getElementById()
-                // template.removeAttr("type");
                 template.addClass('restaurant');
-                //template.val(ui.item.label);
                 template.val(ui.item.value);
                 template.attr('name', 'restaurant[]');
                 template.add('data-id ='.ui.item.value);
-                // template.attr('readonly', true);
                 appendItem(ui.item.label);
-                //template.val(ui.item.label);
                 template.removeAttr("id");
-                //Buttondelete();
-                //  $(this).text("").val("");
                 $('#template-hidden').parent().append(template);
-                //$('#inputRestaurant').text("").val("");
-                // $('#inputRestaurant').reset();
-                // $('#inputRestaurant').removeAttr('value');
             }
-               $('#inputRestaurant').val("");
-                // $("#inputRestaurant").find("input[type=text], textarea").val("");
-                $('#inputRestaurant').value('');
-                //document.getElementById("inputRestaurant").reset();
-            }
+            $('#inputRestaurant').val("");
+            $('#inputRestaurant').value('');
+        }
     };
     $('#inputRestaurant').autocomplete(config);
     $(".btn.btn-default.btn-lg.remove").on('click', deleteItem);
 });
-
 var appendItem = function (text) {
     var ul = $('#content ul');
     var template = ul.find('li:first').clone(true).appendTo(ul);
-    //var template = $('#li-template-hidden').clone();
     template.removeAttr("type");
     template.addClass('restaurant');
     template.append(text);
     template.removeAttr("id");
-//    template.text(text);
-    //  $(this).text("").val("");$('#id-template-hidden').parent().append(template);
 }
-
 var deleteItem = function () {
-    //console.log("function  start")
     var button = $(this);
     var dataId = button.parent().data("id");
     button.parent().remove();
-  //  $(this).parent().remove();
-
-    //console.log($("input[type='hidden']"));
-    //console.log(dataId);
-
     $("input[type=hidden]").each(function () {
-    var InputId = $("input[type='hidden']").data("id");
-
+        var InputId = $("input[type='hidden']").data("id");
         if (InputId == dataId) {
-           // $("input[type='hidden']").data("id").remove();
             $(this).remove();
-
-
         }
     })
 }
-
-
-//  }
-//
-
-//$(document).ready(function(){
-    //$(".btn.btn-default.btn-lg.remove").on(function(){
-    $(".btn.btn-default.btn-lg.remove").on('click', function() {
-        //  $(img).bind('click', function(){
-        var className = $(this).attr('data-id');
-        var allElems = $('".' + className + '"').not(this);
-        //});
-        console.log(className);
-        // $(".btn.btn-default.btn-lg.remove").click(function(){
-        $(this).parent().remove();
-        //var x = document.getElementById("myBtn").getAttribute("onclick")
-        console.log($(this).val($(this).attr('data-id')));
-        var id = $(this).val($(this).attr('data-id'))
-
-        console.log($(this).attr('data-id'));
-        var did = $(this).attr('data-id-');
-
-        console.log($("input").attr('data-id'));
-
-        var element = document.getElementById("data-id");
-        //   element.outerHTML = "";
-        delete element;
-
-        var list = document.getElementsByClassName("restaurant");
-        for (var i = list.length - 1; 0 <= i; i--) {
-            if (list[i] && list[i].parentElement) {
-                list[i].parentElement.removeChild(list[i]);
-            }
-        }
-            //console.log($(this).parent().getAttribute('data-id'));
-            //var attr = $(this).parent().getAttribute('data-id');
-            //console.log(attr);
-            //
-            //var id = $(this).getAttribute('data-id');
-            //$('data-id').attr(attribute);
-            //var element = document.getAnonymousElementByAttribute('data-id', id);
-            //element.parent().remove();
-            //
-            //var id = $(this).attr('#data-id');
-            //console.log($(this).attr('#data-id'));
-            //console.log(id);
-
-            document.getElementsByClassName("restaurant").remove();
-
-
-            //$("#add-file-field").click(function() {
-           //     $("#text").append("<div class='text-field'><input type='text' /> <input type='button' class='remove-btn' value='remove' /></div>");
-           // });
-
-           // $(".remove-btn").live('click',function() {
-                $(this).parent().remove();
-          //  });
-
-        $('data-id').data("3");
-        $("input").remove('*data-id=3');
-
-        jQuery('#data-id=3').remove();
-   // });
-});
